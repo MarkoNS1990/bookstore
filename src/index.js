31f0013c7ebebe5ff3dfa433ebbc2eecdc60f014
Marko Zecevic <marczecevic@gmail.com>
2021-07-09 11:34:58 +0200

    Add book,booksform and bookslist compoonents

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {createStore} from 'redux'
+
+
+
+const store = createStore(reducerCounter)
 
 ReactDOM.render(
   <React.StrictMode>
+  <Provider store = {store}>
     <App />
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
